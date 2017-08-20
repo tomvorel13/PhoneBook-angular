241872bcf15a019e3d99d37d100ef8bb8116c611
Tom Vorel <tomvorel13@gmail.com>
2017-08-20 11:01:47 +0200

    Components created, routing added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ContactListComponent } from './components/contact-list/contact-list.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { AddContactComponent } from './components/add-contact/add-contact.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+
+//Routes
+const appRoutes: Routes = [
+  { path: '', component: DashboardComponent},
+  { path: 'my-contacts', component: ContactListComponent},
+  { path: 'add-contact', component: AddContactComponent}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ContactListComponent,
+    ContactComponent,
+    AddContactComponent,
+    DashboardComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule, RouterModule.forRoot(appRoutes)],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/components/contact-list/contact-list.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-contact-list',
+  templateUrl: './contact-list.component.html',
+  styleUrls: ['./contact-list.component.css']
+})
+export class ContactListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
