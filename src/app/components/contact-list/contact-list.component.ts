21dd1e363d9ea672547ccd3eb38771fb48e3654e
Tom Vorel <tomvorel13@gmail.com>
2017-08-21 12:13:33 +0200

    Simple contact list added, looping through dummy array of contacts

--- src/app/components/contact-list/contact-list.component.ts
+++ src/app/components/contact-list/contact-list.component.ts
@@ -2,15 +2,34 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-contact-list',
-  templateUrl: './contact-list.component.html',
-  styleUrls: ['./contact-list.component.css']
+  selector: "app-contact-list",
+  templateUrl: "./contact-list.component.html",
+  styleUrls: ["./contact-list.component.css"]
 })
 export class ContactListComponent implements OnInit {
+  contacts = [
+    {
+      firstName: "John",
+      lastName: "Malarkey",
+      phone: 123456789
+    },
+    {
+      firstName: "Ben",
+      lastName: "Wilkins",
+      phone: 987456123
+    },
+    {
+      firstName: "Larry",
+      lastName: "Smith",
+      phone: 456789123
+    }
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  onDeleteContact(){
+
   }
-
 }
